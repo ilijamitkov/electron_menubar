8d5e289d2b1cc38e318e54262bd16ab04dd2a74b
ilija <mece7@yahoo.com>
2018-06-25 23:08:35 +0200

    app is ready

--- example/main.js
+++ example/main.js
@@ -6,48 +6,31 @@ const mb = menubar()
 const globalShortcut = electron.globalShortcut
 
 mb.on('ready', function ready () {
-  console.log('app is ready')
-  var screenSize = robotjs.getScreenSize();
-  console.log(screenSize);
-  var mousePosition = robotjs.getMousePos();
-  console.log(mousePosition);
+  //var screenSize = robotjs.getScreenSize();
+  //var mousePosition = robotjs.getMousePos();
   var displays = electron.screen.getAllDisplays();
-  var currentPoint = electron.screen.getCursorScreenPoint();
-  debugger;
-  console.log('current point: ' + currentPoint.x );
-  var currentDisplay = electron.screen.getDisplayNearestPoint(currentPoint)
-  console.log('ccurrentDisplay: ' + currentDisplay.bounds.x);
-  console.log(displays);
-
-  var externalDisplay = displays.find((display) => {
-    return display.bounds.x !== 0 || display.bounds.y !== 0
-  })
-  var savedPoint;
-  var savedPointId;
+  //robotjs.setMouseDelay(20)
+  var pointSaved = false;
+  var previosX, previosY;
   globalShortcut.register('Ctrl+Alt+space', function() {
-    console.log('shortcut activated')
     var currentPoint = electron.screen.getCursorScreenPoint();
     var currentDisplay = electron.screen.getDisplayNearestPoint(currentPoint)
     var nextDisplay = displays.find((display) => {
       return display.bounds.x !== currentDisplay.bounds.x || display.bounds.y !== currentDisplay.bounds.y
     })
-    console.log(nextDisplay)
     if(nextDisplay ===undefined){
         nextDisplay = currentDisplay;
     }
-    console.log('currentDisplayId: ' + currentDisplay.id);
-    console.log('nextDisplay: ' + nextDisplay.id);
-    console.log('savedPoint: ' + savedPoint);
-    savedPoint = savedPoint !== undefined ? savedPoint : {x: (nextDisplay.workArea.width / 2) + nextDisplay.workArea.x, y: (nextDisplay.workArea.height / 2) + nextDisplay.workArea.y }
-    console.log('savedPoint.x: ' + savedPoint.x);
-    console.log('savedPoint.y: ' + nextDisplay.workArea.y);
-    console.log('nextDisplay.workArea.x: ' + nextDisplay.workArea.x);
-    console.log('nextDisplay.workArea.y: ' + nextDisplay.workArea.y);
-    console.log('nextDisplay.workArea.width: ' + nextDisplay.workArea.width);
-    console.log('nextDisplay.workArea.height: ' + nextDisplay.workArea.height);
-    console.log('savedPoint.x: ' + savedPoint.x);
-    console.log('savedPoint.y: ' + nextDisplay.workArea.y);
-    robotjs.moveMouse(savedPoint.x, savedPoint.y)
-    savedPoint = currentPoint;
+    
+    if(!pointSaved){
+      var nextX = parseInt((nextDisplay.bounds.width / 2 + nextDisplay.bounds.x) * nextDisplay.scaleFactor);
+      var nextY = parseInt((nextDisplay.bounds.height / 2 + nextDisplay.bounds.y) * nextDisplay.scaleFactor);
+      robotjs.moveMouse(nextX, nextY);
+    }else {
+      robotjs.moveMouse(previosX, previosY);
+    }
+    pointSaved = true;
+    previosX = currentPoint.x * currentDisplay.scaleFactor;
+    previosY = currentPoint.y * currentDisplay.scaleFactor;
   });
 })
